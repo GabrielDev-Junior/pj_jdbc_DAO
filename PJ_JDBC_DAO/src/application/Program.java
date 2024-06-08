4b007592900616f1aed006661f6e71aa8bba1e98
GbCyber <gabrielavelinoamorim@gmail.com>
2024-06-07 22:52:06 -0300

    Seller entity class

--- PJ_JDBC_DAO/src/application/Program.java
+++ PJ_JDBC_DAO/src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
 import entities.Departments;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Program {
   public static void main(String[] args) {
     Departments obj = new Departments(1,"Books");
     System.out.println(obj);
+    Seller fun = new Seller(1,"Robert","robert@gmail.com",new Date(),3000.0,obj);
+    System.out.println(fun);
   
   }
 }
