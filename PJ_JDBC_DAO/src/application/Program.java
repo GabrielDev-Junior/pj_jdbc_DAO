33c8a31e358d42a19c1f4a686efeaec10ef8301d
GbCyber <gabrielavelinoamorim@gmail.com>
2024-06-11 23:03:15 -0300

    findAll() implementation

--- PJ_JDBC_DAO/src/application/Program.java
+++ PJ_JDBC_DAO/src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
     
-    System.out.print("Informe o id para busca: ");
+    System.out.print("Teste 1 Informe o id para busca: ");
     int id = sc.nextInt();
     
     SellerDao sellerDao = DaoFactory.createSellerDao();
@@ -25,11 +25,21 @@ public class Program {
     System.out.println("Resultado: ");
     System.out.println(seller);
     
-    System.out.println("Id para busca por Department: ");
+    System.out.println("Teste 2 Id para busca por Department: ");
     Departments dep = new Departments(2,null);
     
     List<Seller> list = sellerDao.findByDepartment(dep);
     
+    for(Seller obj : list){
+      if (list.isEmpty()){
+        System.out.println("Lista está vazia");
+      }
+      System.out.println(obj);
+    }
+    
+    System.out.println(" Teste 3 findAll: ");
+    
+    list = sellerDao.findAll();
     for(Seller obj : list){
       if (list.isEmpty()){
         System.out.println("Lista está vazia");
